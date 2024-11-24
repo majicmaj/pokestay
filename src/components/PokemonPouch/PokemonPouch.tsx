77e05efaf6543cd0dde93944ae0cea56bee004bf
majicmaj <majd.tarbin@outlook.com>
2024-11-23 23:47:06 -0500

    Refactor PokemonPouch component: improve code readability and structure; enhance sorting and filtering logic

--- src/components/PokemonPouch/PokemonPouch.tsx
+++ src/components/PokemonPouch/PokemonPouch.tsx
@@ -1,107 +1,137 @@
-import React, { useState } from 'react';
-import { Sparkles } from 'lucide-react';
-import { Pokemon } from '../../types';
-import SelectedPokemon from './SelectedPokemon';
-import useGameState from '../../hooks/useGameState';
+import { Sparkles } from "lucide-react";
+import React, { useState } from "react";
+import useGameState from "../../hooks/useGameState";
+import { Pokemon } from "../../types";
+import SelectedPokemon from "./SelectedPokemon";
 
 interface PokemonPouchProps {
   // onSelectBuddy: (pokemon: Pokemon) => void;
   // currentBuddy: Pokemon | null;
 }
 
-type SortBy = 'level' | 'recent' | 'name' | 'cp' | 'id'
-const PokemonPouch: React.FC<PokemonPouchProps> = ({
-}) => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [sortBy, setSortBy] = useState<SortBy>('recent');
-  const [selectedPokemon, setSelectedPokemon] = useState<Pokemon | null>(null)
+type SortBy = "level" | "recent" | "name" | "cp" | "id";
+const PokemonPouch: React.FC<PokemonPouchProps> = ({}) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortBy, setSortBy] = useState<SortBy>("recent");
+  const [selectedPokemon, setSelectedPokemon] = useState<Pokemon | null>(null);
 
-  const [gameState] = useGameState()
-  const {buddyPokemon, inventory = []} = gameState || {}
+  const [gameState] = useGameState();
+  const { buddyPokemon, inventory = [] } = gameState || {};
 
   const sortedPokemon = [...inventory]?.sort((a, b) => {
     switch (sortBy) {
-      case 'cp':
+      case "cp":
         return (b.cp || 0) - (a.cp || 0);
-      case 'id':
+      case "id":
         return a.id - b.id;
-      case 'level':
+      case "level":
         return b.stats.level - a.stats.level;
-      case 'name':
+      case "name":
         return a.name.localeCompare(b.name);
-      case 'recent':
+      case "recent":
       default:
-      return 0; // Assumes the array is already in chronological order
+        return 0; // Assumes the array is already in chronological order
     }
   });
 
-  const filteredPokemon = sortedPokemon.filter(pokemon =>
-    pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    pokemon.types.some((type: string) => type.toLowerCase().includes(searchTerm.toLowerCase()))
+  const filteredPokemon = sortedPokemon.filter(
+    (pokemon) =>
+      pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      pokemon.types.some((type: string) =>
+        type.toLowerCase().includes(searchTerm.toLowerCase())
+      )
   );
 
+  const buddyIndex = buddyPokemon
+    ? filteredPokemon.findIndex(
+        (p) =>
+          p.id === buddyPokemon.id && p.stats.level === buddyPokemon.stats.level
+      )
+    : -1;
+
   return (
-    <div className="h-screen overflow-auto flex flex-col">
-        {selectedPokemon && <div className='absolute top-0 left-0 grid place-items-center h-full w-full p-8 bg-black/20 filter backdrop-blur z-20'>
-          <div className='h-full w-full'>
-          <SelectedPokemon pokemon={selectedPokemon}
-          setSelectedPokemon={setSelectedPokemon}
-          />
+    <div className="h-screen overflow-auto flex flex-col items-center">
+      {selectedPokemon && (
+        <div className="absolute top-0 left-0 grid place-items-center h-full w-full p-8 bg-black/20 filter backdrop-blur z-20">
+          <div className="h-full w-full">
+            <SelectedPokemon
+              pokemon={selectedPokemon}
+              setSelectedPokemon={setSelectedPokemon}
+            />
           </div>
-        </div>}
-      {/* <h1 className='text-md pt-8 text-center'>POKEMON</h1> */}
-      <div className='flex flex-col text-center pt-4' onClick={() => setSelectedPokemon(buddyPokemon)}>
-          <span className='font-medium underline'>
-            {buddyPokemon?.name || "None"}
-          </span>
-          <span className='font-light text-sm opacity-60 pr-2'>Current Buddy</span>
-          </div>
-      <div className='p-4 flex flex-col gap-2'>
-        <input 
+        </div>
+      )}
+      <h1 className="text-md pt-8 text-center">POKEMON</h1>
+      <div className="p-4 flex flex-col gap-2">
+        <input
           value={searchTerm}
-          onChange={e => setSearchTerm(e.target.value)}
-          className='px-3 py-1 bg-lime-200 w-full rounded-full'
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="px-3 py-1 bg-lime-200 w-full rounded-full"
         />
-        <div className='flex w-full gap-4 text-sm'>
-          <p className='opacity-60'>Sort By</p>
-          <button className={sortBy === 'cp' ? 'underline' : ''} onClick={() => setSortBy('cp')}>CP</button>
-          <button className={sortBy === 'level' ? 'underline' : ''} onClick={() => setSortBy('level')}>Level</button>
-          <button className={sortBy === 'name' ? 'underline' : ''} onClick={() => setSortBy('name')}>Name</button>
-          <button className={sortBy === 'id' ? 'underline' : ''} onClick={() => setSortBy('id')}>ID</button>
-          <button className={sortBy === 'recent' ? 'underline' : ''} onClick={() => setSortBy('recent')}>Recent</button>
+        <div className="flex w-full gap-4 text-sm">
+          <p className="opacity-60">Sort By</p>
+          <button
+            className={sortBy === "cp" ? "underline" : ""}
+            onClick={() => setSortBy("cp")}
+          >
+            CP
+          </button>
+          <button
+            className={sortBy === "level" ? "underline" : ""}
+            onClick={() => setSortBy("level")}
+          >
+            Level
+          </button>
+          <button
+            className={sortBy === "name" ? "underline" : ""}
+            onClick={() => setSortBy("name")}
+          >
+            Name
+          </button>
+          <button
+            className={sortBy === "id" ? "underline" : ""}
+            onClick={() => setSortBy("id")}
+          >
+            ID
+          </button>
+          <button
+            className={sortBy === "recent" ? "underline" : ""}
+            onClick={() => setSortBy("recent")}
+          >
+            Recent
+          </button>
         </div>
       </div>
-        <div className="grid max-w-[900px] pb-24 pt-4 gap-4 grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6gap-3">
-          {filteredPokemon.map((pokemon) => (
-            <div
-              key={`${pokemon.id}-${pokemon.stats.level}`}
-              className="flex flex-col items-center"
-              onClick={() => setSelectedPokemon(pokemon)}
-            >
-              <div className='mb-[-12px]'>
-                <span className="text-sm font-medium opacity-60 pr-1">
-                  CP
-                </span>
+      <div className="grid max-w-[900px] px-4 pb-24 pt-4 gap-4 grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6gap-3">
+        {filteredPokemon.map((pokemon, i) => (
+          <div
+            key={`${pokemon.id}-${pokemon.stats.level}`}
+            className={`flex flex-col items-center ${
+              buddyIndex === i ? "bg-lime-200/80" : ""
+            } rounded-lg p-2 cursor-pointer`}
+            onClick={() => setSelectedPokemon(pokemon)}
+          >
+            <div className="mb-[-12px]">
+              <span className="text-sm font-medium opacity-60 pr-1">CP</span>
 
-                <span className="text-xl font-bold">
-                  {pokemon.cp}
-                </span>
-              </div>
-              <div className="h-full w-full aspect-square flex items-center justify-center">
-                <img
-                  src={pokemon.sprite}
-                  alt={pokemon.name}
-                  className="pixelated w-full h-full aspect-square object-contain"
-                />
-              </div>
-              <div className="font-semibold mt-[-16px] flex items-center">{pokemon.isShiny && <Sparkles className='w-4' />}{pokemon.name}</div>
+              <span className="text-xl font-bold">{pokemon.cp}</span>
             </div>
-          ))}
-        </div>
-
+            <div className="h-full w-full aspect-square flex items-center justify-center">
+              <img
+                src={pokemon.sprite}
+                alt={pokemon.name}
+                className="pixelated w-full h-full aspect-square object-contain"
+              />
+            </div>
+            <div className="font-semibold mt-[-16px] flex items-center">
+              {pokemon.isShiny && <Sparkles className="w-4" />}
+              {pokemon.name}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
-
 
 export default PokemonPouch;
--- src/components/PokemonPouch/SelectedPokemon.tsx
+++ src/components/PokemonPouch/SelectedPokemon.tsx
@@ -102,145 +102,151 @@ const SelectedPokemon = ({
   };
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="absolute top-0 w-full flex px-8 flex-col gap-4 h-full items-center">
-        <div className="absolute -z-10 top-80 h-full rounded-xl w-[calc(100%-32px)] border aspect-square  bg-white drop-shadow-xl p-8" />
-
-        <div className="relative w-96 flex pt-8 h-96 flex-col justify-between items-center">
-          <div className="text-white">
-            <span className="text-sm font-medium opacity-60 pr-1">CP</span>
-            <span className="text-4xl">{pokemon.cp}</span>
-          </div>
-          <img
-            src={pokemon.sprite}
-            alt={pokemon.name}
-            className="pixelated absolute p-12 bottom-0 w-96 h-96 object-contain"
-          />
-          <div className="flex items-center flex-col">
-            <div className="font-semibold text-3xl mt-[-16px] flex items-center">
-              {pokemon.isShiny && <Sparkles className="w-8" />}
-              {pokemon.name}
+    <div className="flex justify-center items-center">
+      <div className="absolute h-screen w-screen overflow-auto top-0 flex px-8 flex-col gap-4 items-center">
+        <div className="relative top-[200px] bottom-0 grid h-full min-h-max rounded-t-xl w-full bg-white drop-shadow-xl">
+          <div className="relative top-[-200px] bottom-0 h-full flex flex-col min-h-max items-center gap-4 overflow-auto px-8">
+            <div className="relative w-96 flex pt-8 h-96 flex-col justify-between items-center">
+              <div className="text-white">
+                <span className="text-sm font-medium opacity-60 pr-1">CP</span>
+                <span className="text-4xl">{pokemon.cp}</span>
+              </div>
+              <img
+                src={pokemon.sprite}
+                alt={pokemon.name}
+                className="pixelated absolute p-12 bottom-0 w-96 h-96 object-contain"
+              />
+              <div className="flex items-center flex-col">
+                <div className="font-semibold text-3xl mt-[-16px] flex items-center">
+                  {pokemon.isShiny && <Sparkles className="w-8" />}
+                  {pokemon.name}
+                </div>
+                <p className="text-sm">
+                  {pokemon.stats.hp} HP / Lv.{pokemon.stats.level}
+                </p>
+              </div>
             </div>
-            <p className="text-sm">
-              {pokemon.stats.hp} HP / Lv.{pokemon.stats.level}
-            </p>
-          </div>
-        </div>
 
-        <div className="flex rounded-full border-2">
-          {pokemon.types.map((type) => (
-            <TypeBadge key={type} type={type} />
-          ))}
-        </div>
+            <div className="flex rounded-full border-2">
+              {pokemon.types.map((type) => (
+                <TypeBadge key={type} type={type} />
+              ))}
+            </div>
 
-        <div className="border-b-2 border-zinc-300 w-full" />
+            <div className="border-b-2 border-zinc-300 w-full" />
 
-        <div className="grid grid-cols-3 place-items-center gap-4 px-4 w-full">
-          <div className="grid place-items-center">
-            <p className="font-bold text-md">{pokemon.stats.attack}</p>
-            <p className="opacity-80 text-sm">Attack</p>
-          </div>
-          <div className="grid place-items-center">
-            <p className="font-bold text-md">{pokemon.stats.defense}</p>
-            <p className="opacity-80 text-sm">Defense</p>
-          </div>
-          <div className="grid place-items-center">
-            <p className="font-bold text-md">{pokemon.stats.speed}</p>
-            <p className="opacity-80 text-sm">Speed</p>
-          </div>
-        </div>
+            <div className="grid grid-cols-3 place-items-center gap-4 px-4 pb-4 w-full">
+              <div className="grid place-items-center">
+                <p className="font-bold text-md">{pokemon.stats.attack}</p>
+                <p className="opacity-80 text-sm">Attack</p>
+              </div>
+              <div className="grid place-items-center">
+                <p className="font-bold text-md">{pokemon.stats.defense}</p>
+                <p className="opacity-80 text-sm">Defense</p>
+              </div>
+              <div className="grid place-items-center">
+                <p className="font-bold text-md">{pokemon.stats.speed}</p>
+                <p className="opacity-80 text-sm">Speed</p>
+              </div>
+            </div>
 
-        {/* Level Up */}
-        <div className="rounded-full pr-4 items-center justify-between flex gap-4 bg-lime-100/80 w-full">
-          <button
-            onClick={levelUp}
-            disabled={isLevelUpDisabled}
-            className={`rounded-full w-40 justify-center text-xl font-medium text-white bg-teal-500 bg-gradient-to-r from-lime-500 to-teal-500 gap-2 flex items-center px-8 py-3 ${
-              isLevelUpDisabled && "opacity-50 cursor-not-allowed"
-            }`}
-          >
-            LEVEL UP
-          </button>
-          {/* <button
+            {/* Level Up */}
+            <div className="rounded-full pr-4 items-center justify-between flex gap-4 bg-lime-100/80 w-full">
+              <button
+                onClick={levelUp}
+                disabled={isLevelUpDisabled}
+                className={`rounded-full w-40 justify-center text-xl font-medium text-white bg-teal-500 bg-gradient-to-r from-lime-500 to-teal-500 gap-2 flex items-center px-8 py-3 ${
+                  isLevelUpDisabled && "opacity-50 cursor-not-allowed"
+                }`}
+              >
+                LEVEL UP
+              </button>
+              {/* <button
             onClick={add1000Points}
             className={`rounded-full text-xl text-white bg-teal-500 bg-gradient-to-r from-lime-500 to-teal-500 gap-2 flex items-center px-8 py-3`}
           >
             +1000
           </button> */}
-          <div className="flex flex-1 items-center gap-1">
-            <Sparkle className="h-4 w-4" />
-            <span className={`${isLevelUpDisabled && "text-red-500"}`}>
-              {formatNumber(levelUpCost)}
-            </span>
-            <span className="opacity-70">/ {formatNumber(points)}</span>
+              <div className="flex flex-1 items-center gap-1">
+                <Sparkle className="h-4 w-4" />
+                <span className={`${isLevelUpDisabled && "text-red-500"}`}>
+                  {formatNumber(levelUpCost)}
+                </span>
+                <span className="opacity-70">/ {formatNumber(points)}</span>
+              </div>
+            </div>
+
+            {/* Evolution */}
+            <div className="rounded-full pr-4 items-center justify-between flex gap-4 bg-lime-100/80 w-full">
+              {/* Pink magenta button */}
+              <button
+                onClick={evolve}
+                disabled={!canEvolve}
+                className={`rounded-full w-40 justify-center text-xl font-medium text-white bg-teal-500 bg-gradient-to-r from-pink-500 to-purple-500 gap-2 flex items-center px-8 py-3 ${
+                  !canEvolve &&
+                  "opacity-50 cursor-not-allowed border-3 bored-red-500"
+                }`}
+              >
+                EVOLVE
+              </button>
+              <div className="flex flex-1 items-center gap-1">
+                <Sparkle className="h-4 w-4" />
+                <span className={`${!canEvolve && "text-red-500"}`}>
+                  {formatNumber(evolutionCost)}
+                </span>
+                <span className="opacity-70">/ {formatNumber(points)}</span>
+              </div>
+            </div>
+
+            {/* Transfer */}
+            <div className="rounded-full pr-4 items-center justify-between flex gap-4 bg-lime-100/80 w-full">
+              {!confirmTransfer && (
+                <button
+                  onClick={() => setConfirmTransfer(true)}
+                  className={`rounded-full w-40 justify-center text-xl font-medium text-white bg-teal-500 bg-gradient-to-r from-red-500 to-rose-500 gap-2 flex items-center px-8 py-3`}
+                >
+                  TRANSFER
+                </button>
+              )}
+              {confirmTransfer && (
+                <button
+                  onClick={transferPokemon}
+                  className={`rounded-full w-40 justify-center text-xl font-medium text-white bg-teal-500 bg-gradient-to-r from-red-500 to-rose-500 gap-2 flex items-center px-8 py-3`}
+                >
+                  Yes
+                </button>
+              )}
+              {confirmTransfer && (
+                <button
+                  onClick={() => setConfirmTransfer(false)}
+                  className={`rounded-full w-40 justify-center text-xl font-medium text-white bg-teal-500 bg-gradient-to-r from-lime-500 to-teal-500 gap-2 flex items-center px-8 py-3`}
+                >
+                  CANCEL
+                </button>
+              )}
+              <div className="flex flex-1 items-center gap-1">
+                <Sparkle className="h-4 w-4" />
+                <span>+{formatNumber(transferStardust)}</span>
+              </div>
+            </div>
+
+            <button
+              onClick={isBuddyPokemon ? handleRemoveBuddy : handleMakeBuddy}
+              className={`w-64 justify-center rounded-full text-xl border-2 gap-2 flex items-center px-6 py-3 transition-colors ${
+                isBuddyPokemon
+                  ? "bg-lime-200 border-lime-300 bg-gradient-to-r from-lime-200 to-teal-200 drop-shadow-lg"
+                  : "border-teal-800 bg-white drop-shadow-none"
+              }`}
+            >
+              {isBuddyPokemon ? (
+                <UserCheck className="h-6 w-6" />
+              ) : (
+                <UserPlus className="h-6 w-6" />
+              )}
+              {isBuddyPokemon ? "Current Buddy" : "Select as Buddy"}
+            </button>
           </div>
         </div>
-
-        {/* Evolution */}
-        <div className="rounded-full pr-4 items-center justify-between flex gap-4 bg-lime-100/80 w-full">
-          {/* Pink magenta button */}
-          <button
-            onClick={evolve}
-            disabled={!canEvolve}
-            className={`rounded-full w-40 justify-center text-xl font-medium text-white bg-teal-500 bg-gradient-to-r from-pink-500 to-purple-500 gap-2 flex items-center px-8 py-3 ${
-              !canEvolve &&
-              "opacity-50 cursor-not-allowed border-3 bored-red-500"
-            }`}
-          >
-            EVOLVE
-          </button>
-          <div className="flex flex-1 items-center gap-1">
-            <Sparkle className="h-4 w-4" />
-            <span className={`${!canEvolve && "text-red-500"}`}>
-              {formatNumber(evolutionCost)}
-            </span>
-            <span className="opacity-70">/ {formatNumber(points)}</span>
-          </div>
-        </div>
-
-        {/* Transfer */}
-        <div className="rounded-full pr-4 items-center justify-between flex gap-4 bg-lime-100/80 w-full">
-          {!confirmTransfer && (
-            <button
-              onClick={() => setConfirmTransfer(true)}
-              className={`rounded-full w-40 justify-center text-xl font-medium text-white bg-teal-500 bg-gradient-to-r from-red-500 to-rose-500 gap-2 flex items-center px-8 py-3`}
-            >
-              TRANSFER
-            </button>
-          )}
-          {confirmTransfer && (
-            <button
-              onClick={transferPokemon}
-              className={`rounded-full w-40 justify-center text-xl font-medium text-white bg-teal-500 bg-gradient-to-r from-red-500 to-rose-500 gap-2 flex items-center px-8 py-3`}
-            >
-              Yes
-            </button>
-          )}
-          {confirmTransfer && (
-            <button
-              onClick={() => setConfirmTransfer(false)}
-              className={`rounded-full w-40 justify-center text-xl font-medium text-white bg-teal-500 bg-gradient-to-r from-lime-500 to-teal-500 gap-2 flex items-center px-8 py-3`}
-            >
-              CANCEL
-            </button>
-          )}
-          <div className="flex flex-1 items-center gap-1">
-            <Sparkle className="h-4 w-4" />
-            <span>+{formatNumber(transferStardust)}</span>
-          </div>
-        </div>
-
-        <button
-          onClick={isBuddyPokemon ? handleRemoveBuddy : handleMakeBuddy}
-          className="rounded-full text-sm border-2 border-teal-800 gap-2 flex items-center px-3 py-1"
-        >
-          {isBuddyPokemon ? (
-            <UserCheck className="h-6 w-6" />
-          ) : (
-            <UserPlus className="h-6 w-6" />
-          )}
-          {isBuddyPokemon ? "Current Buddy" : "Select as Buddy"}
-        </button>
       </div>
 
       <button
