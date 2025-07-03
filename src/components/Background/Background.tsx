26ddd5fb540d37e3054dfb7e10de73888784bb54
majicmaj <majd.tarbin@outlook.com>
2025-07-03 14:52:26 -0400

    Add new background images and update Background component to support dynamic backgrounds based on Pokémon type
    Introduce new background images for various Pokémon types and modify the Background component to dynamically select the appropriate background image based on the current Pokémon type. This enhances the visual experience and provides a more immersive environment for users.

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -3,9 +3,51 @@ import { Particles } from "react-tsparticles";
 import { Engine } from "tsparticles-engine";
 import { loadSlim } from "tsparticles-slim"; // Load all tsparticles plugins
 import bg from "../../assets/background/bg.png";
+import darkBg from "../../assets/background/dark.png";
+import electricBg from "../../assets/background/electric.png";
+import fairyBg from "../../assets/background/fairy.png";
+import fightingBg from "../../assets/background/fighting.png";
+import fireBg from "../../assets/background/fire.png";
+import iceBg from "../../assets/background/ice.png";
+import rockBg from "../../assets/background/rock.png";
+import waterBg from "../../assets/background/water.png";
+
 import { WildPokemonState } from "../../types";
 import { getBackgroundColor } from "../../utils/getBackgroundColor";
 import getBackgroundParticles from "../../utils/getBackgroundParticles";
+
+// const allPokemonTypes = [
+//   "grass",
+//   "poison",
+//   "fire",
+//   "water",
+//   "electric",
+//   "fighting",
+//   "fairy",
+//   "psychic",
+//   "bug",
+//   "rock",
+//   "ghost",
+//   "dragon",
+//   "dark",
+//   "steel",
+//   "normal",
+//   "flying",
+//   "ground",
+//   "ice",
+// ];
+
+const getBg = (pokemonType: string) => {
+  if (pokemonType === "dark") return darkBg;
+  if (pokemonType === "electric") return electricBg;
+  if (pokemonType === "fairy") return fairyBg;
+  if (pokemonType === "fighting") return fightingBg;
+  if (pokemonType === "fire") return fireBg;
+  if (pokemonType === "ice") return iceBg;
+  if (pokemonType === "water") return waterBg;
+  if (pokemonType === "rock") return rockBg;
+  return bg;
+};
 
 const Background = ({
   currentPokemon,
@@ -28,7 +70,10 @@ const Background = ({
       <div
         className={`bg-gradient-to-b top-0 absolute from-sky-200/90 to-blue-300/90 dark:from-sky-800/90 dark:to-blue-900/90 h-1/2 w-full`}
       />
-      <img className="absolute pixelated w-full h-full object-cover" src={bg} />
+      <img
+        className="absolute pixelated w-full h-full object-cover"
+        src={getBg(pokemonType ?? "default")}
+      />
       <div
         className={`mix-blend-hue transition-colors top-[47%] h-full w-full absolute opacity-10 ${bgColor} `}
       />
