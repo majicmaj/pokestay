1dbf6145f52848cee5b0ed43f94c5af3f7320ecc
majicmaj <majd.tarbin@outlook.com>
2025-07-03 10:23:59 -0400

    Refactor Header layout for improved spacing; change from grid to flex for better alignment and adjust padding. Update SlidingMenus component to enhance styling with rounded corners and margin adjustments.

--- src/components/PokemonPouch/Header.tsx
+++ src/components/PokemonPouch/Header.tsx
@@ -8,12 +8,11 @@ interface HeaderSectionProps {
 
 const Header: React.FC<HeaderSectionProps> = ({ inventoryCount, points }) => {
   return (
-    <div className="pt-8 w-full grid grid-cols-3 gap-4 place-items-center bg-primary p-4 rounded-b-xl">
+    <div className="w-full flex items-center justify-between px-4 gap-4 place-items-center p-2">
       <div className="flex items-center gap-1">
         <Pokeball className="w-6 h-6" />
         {inventoryCount}
       </div>
-      <h1 className="text-md text-center font-bold">POKEMON</h1>
       <div className="flex items-center gap-1">
         <Stardust className="w-6 h-6" />
         {points}
--- src/components/SlidingMenus/SlidingMenus.tsx
+++ src/components/SlidingMenus/SlidingMenus.tsx
@@ -33,7 +33,7 @@ const SlidingMenus = ({ handleFlee }: { handleFlee: () => void }) => {
             : "translate-y-full opacity-0"
         }`}
       >
-        <div className="bg-background h-full w-full text-accent-content dark:bg-dark-primary dark:text-dark-text">
+        <div className="mt-2 rounded-t-xl overflow-hidden bg-background h-full w-full text-accent-content dark:bg-dark-primary dark:text-dark-text">
           <button
             className="z-20 absolute bottom-8 left-1/2 -translate-x-1/2 rounded-full bg-teal-600 p-2 text-lime-200 border border-lime-200"
             onClick={() => setActiveMenu("none")}
